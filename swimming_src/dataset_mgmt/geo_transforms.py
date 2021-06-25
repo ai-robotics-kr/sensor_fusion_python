a87d6e37b658128e584ee4ae41959d014b456b85
kimsooyoung <tge1375@naver.com>
2021-06-26 04:03:18 +0900

    normalize_angles location changed

--- swimming_src/dataset_mgmt/geo_transforms.py
+++ swimming_src/dataset_mgmt/geo_transforms.py
@@ -129,3 +129,13 @@ def enu_to_lla(points_enu, ref_lla):
     points_ecef = enu_to_ecef(points_enu, ref_lla)
     points_lla = ecef_to_lla(points_ecef)
     return points_lla
+
+def normalize_angles(angles):
+    """
+    Args:
+        angles (float or numpy.array): angles in radian (= [a1, a2, ...], shape of [n,])
+    Returns:
+        numpy.array or float: angles in radians normalized b/w/ -pi and +pi (same shape w/ angles)
+    """
+    angles = (angles + np.pi) % (2 * np.pi) - np.pi
+    return angles
