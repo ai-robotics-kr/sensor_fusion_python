63ca3a8085c59fa6c619857ce315eb0964242d91
kimsooyoung <tge1375@naver.com>
2021-06-26 04:03:31 +0900

    Dataset visualization  test done

--- swimming_src/dataset_mgmt/kitti_manager.py
+++ swimming_src/dataset_mgmt/kitti_manager.py
@@ -4,28 +4,17 @@ import pykitti
 
 from geo_transforms import lla_to_enu
 
-
-def normalize_angles(angles):
-    """
-    Args:
-        angles (float or numpy.array): angles in radian (= [a1, a2, ...], shape of [n,])
-    Returns:
-        numpy.array or float: angles in radians normalized b/w/ -pi and +pi (same shape w/ angles)
-    """
-    angles = (angles + np.pi) % (2 * np.pi) - np.pi
-    return angles
-
 class KittiDatasetMgmt(object):
     def __init__(self, kitti_root_dir, kitti_date, kitti_drive):
         super().__init__()
 
         self.dataset = pykitti.raw(kitti_root_dir, kitti_date, kitti_drive)
 
-        # Ground Truth Values
-        self.gt_forward_velocities = []
+        # [longitude(deg), latitude(deg), altitude(meter)] x N
         self.gt_trajectory_lla = []
-        self.gt_yaw_rates = []
-        self.gt_yaws = []
+        self.gt_yaws = []  # [yaw_angle(rad),] x N
+        self.gt_yaw_rates = []  # [vehicle_yaw_rate(rad/s),] x N
+        self.gt_forward_velocities = []  # [vehicle_forward_velocity(m/s),] x N
 
         self.generateGroundTruthSets()
 
@@ -37,23 +26,18 @@ class KittiDatasetMgmt(object):
         self.dataset = pykitti.raw(kitti_root_dir, kitti_date, kitti_drive)
 
     def generateGroundTruthSets(self):
-        # [longitude(deg), latitude(deg), altitude(meter)] x N
-        gt_trajectory_lla = []
-        gt_yaws = []  # [yaw_angle(rad),] x N
-        gt_yaw_rates = []  # [vehicle_yaw_rate(rad/s),] x N
-        gt_forward_velocities = []  # [vehicle_forward_velocity(m/s),] x N
 
         for oxts_data in self.dataset.oxts:
             packet = oxts_data.packet
-            gt_trajectory_lla.append([packet.lon, packet.lat, packet.alt])
-            gt_yaws.append(packet.yaw)
-            gt_yaw_rates.append(packet.wz)
-            gt_forward_velocities.append(packet.vf)
+            self.gt_trajectory_lla.append([packet.lon, packet.lat, packet.alt])
+            self.gt_yaws.append(packet.yaw)
+            self.gt_yaw_rates.append(packet.wz)
+            self.gt_forward_velocities.append(packet.vf)
 
-        gt_trajectory_lla = np.array(gt_trajectory_lla).T
-        gt_yaws = np.array(gt_yaws)
-        gt_yaw_rates = np.array(gt_yaw_rates)
-        gt_forward_velocities = np.array(gt_forward_velocities)
+        self.gt_trajectory_lla = np.array(self.gt_trajectory_lla).T
+        self.gt_yaws = np.array(self.gt_yaws)
+        self.gt_yaw_rates = np.array(self.gt_yaw_rates)
+        self.gt_forward_velocities = np.array(self.gt_forward_velocities)
 
     def getDatasetShape(self):
         """TODO: shape for all data types??
@@ -74,19 +58,32 @@ class KittiDatasetMgmt(object):
 
         fig, ax = plt.subplots(1, 1, figsize=(8, 6))
         ax.plot(lons, lats)
+        plt.title('GPS trajactory')
         ax.set_xlabel('longitude [deg]')
         ax.set_ylabel('latitude [deg]')
         ax.grid()
-        ax.show()
+        plt.show()
+
+    def plotXYZtrajactory(self):
+        xs, ys, _ = self.gt_trajectory_xyz
+        
+        fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+        ax.plot(xs, ys)
+        plt.title('XYZ trajactory')
+        ax.set_xlabel('X [m]')
+        ax.set_ylabel('Y [m]')
+        ax.grid()
+        plt.show()
 
 if __name__ == "__main__":
-    kitti_root_dir = "/home/kimsooyoung/Documents/AI_KR" # Put your dataset location
+    kitti_root_dir = "/home/swimming/Documents/Dataset" # Put your dataset location
     kitti_date = "2011_09_30"
     kitti_drive = "0033"
     
     test_mgmt = KittiDatasetMgmt(kitti_root_dir, kitti_date, kitti_drive)
     
     test_mgmt.plotGPStrajactory()
+    test_mgmt.plotXYZtrajactory()
 
     # Unit Test Here
     pass
