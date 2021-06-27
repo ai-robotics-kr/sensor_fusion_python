766c18e8ebf69f78c410139cb403499a53e45ba8
swimming_kim <tge1375@naver.com>
2021-06-27 21:53:45 +0900

    Feature : plotEstimated2DStates
    Signed-off-by: swimming_kim <tge1375@naver.com>

--- swimming_src/visualize/result_viz.py
+++ swimming_src/visualize/result_viz.py
@@ -11,32 +11,128 @@ class ResultVizPlotter(object):
         fig, ax = plt.subplots(1, 1, figsize=(12, 9))
 
         gt_xs, gt_ys, _ = groud_truth
-        ax.plot(gt_xs, gt_ys, lw=2, label='ground-truth trajectory')
+        ax.plot(gt_xs, gt_ys, lw=2, label="ground-truth trajectory")
 
         noisy_xs, noisy_ys, _ = noisy_traj
-        ax.plot(noisy_xs, noisy_ys, lw=0, marker='.', markersize=4, alpha=1., label='observed trajectory')
+        ax.plot(
+            noisy_xs,
+            noisy_ys,
+            lw=0,
+            marker=".",
+            markersize=4,
+            alpha=1.0,
+            label="observed trajectory",
+        )
 
         est_xs, ext_ys, _ = estimated_traj
-        ax.plot(est_xs, ext_ys, lw=2, label='estimated trajectory', color='r')
+        ax.plot(est_xs, ext_ys, lw=2, label="estimated trajectory", color="r")
 
         plt.title("Trajactory Comparison")
-        ax.set_xlabel('X [m]')
-        ax.set_ylabel('Y [m]')
+        ax.set_xlabel("X [m]")
+        ax.set_ylabel("Y [m]")
         ax.legend()
         ax.grid()
 
         plt.show()
 
-    def plotEstimated2DStates(self, ts, gt_trajectory_xyz, estimated_traj, estimated_var):
+    def plotEstimated2DStates(
+        self, ts, gt_trajectory_xyz, estimated_traj, estimated_var, gt_yaws
+    ):
         fig, ax = plt.subplots(2, 3, figsize=(14, 6))
 
-        ax[0, 0].plot(ts, gt_trajectory_xyz[0], lw=2, label='ground-truth')
-        ax[0, 0].plot(ts, estimated_traj[0], lw=1, label='estimated', color='r')
+        # Analyze estimation error of X
+        ax[0, 0].plot(ts, gt_trajectory_xyz[0], lw=2, label="ground-truth")
+        ax[0, 0].plot(ts, estimated_traj[0], lw=1, label="estimated", color="r")
         ax[0, 0].set_xlabel("time elapsed [sec]")
         ax[0, 0].set_ylabel("X [m]")
         ax[0, 0].legend()
-        
+
+        ax[1, 0].plot(
+            ts,
+            estimated_traj[0] - gt_trajectory_xyz[0],
+            lw=1.5,
+            label="estimation error",
+        )
+        ax[1, 0].plot(
+            ts,
+            np.sqrt(estimated_var[0]),
+            lw=1.5,
+            label="estimated 1-sigma interval",
+            color="darkorange",
+        )
+        ax[1, 0].plot(
+            ts, -np.sqrt(estimated_var[0]), lw=1.5, label="", color="darkorange"
+        )
+        ax[1, 0].set_xlabel("time elapsed [sec]")
+        ax[1, 0].set_ylabel("X estimation error [m]")
+        ax[1, 0].legend()
+
+        # Analyze estimation error of Y
+        ax[0, 1].plot(ts, gt_trajectory_xyz[1], lw=2, label="ground-truth")
+        ax[0, 1].plot(ts, estimated_traj[1], lw=1, label="estimated", color="r")
+        ax[0, 1].set_xlabel("time elapsed [sec]")
+        ax[0, 1].set_ylabel("Y [m]")
+        ax[0, 1].legend()
+
+        ax[1, 1].plot(
+            ts,
+            estimated_traj[1] - gt_trajectory_xyz[1],
+            lw=1.5,
+            label="estimation error",
+        )
+        ax[1, 1].plot(
+            ts,
+            np.sqrt(estimated_var[1]),
+            lw=1.5,
+            label="estimated 1-sigma interval",
+            color="darkorange",
+        )
+        ax[1, 1].plot(
+            ts, -np.sqrt(estimated_var[1]), lw=1.5, label="", color="darkorange"
+        )
+        ax[1, 1].set_xlabel("time elapsed [sec]")
+        ax[1, 1].set_ylabel("Y estimation error [m]")
+        ax[1, 1].legend()
+
+        # Analyze estimation error of Theta
+        ax[0, 2].plot(ts, gt_trajectory_xyz[2], lw=2, label="ground-truth")
+        ax[0, 2].plot(ts, estimated_traj[2], lw=1, label="estimated", color="r")
+        ax[0, 2].set_xlabel("time elapsed [sec]")
+        ax[0, 2].set_ylabel("yaw angle [rad/s]")
+        ax[0, 2].legend()
+
+        ax[1, 2].plot(
+            ts,
+            self.normalize_angles(estimated_traj[2] - gt_yaws),
+            lw=1.5,
+            label="estimation error",
+        )
+        ax[1, 2].plot(
+            ts,
+            np.sqrt(estimated_var[2]),
+            lw=1.5,
+            label="estimated 1-sigma interval",
+            color="darkorange",
+        )
+        ax[1, 2].plot(
+            ts, -np.sqrt(estimated_var[2]), lw=1.5, label="", color="darkorange"
+        )
+        ax[1, 2].set_ylim(-0.5, 0.5)
+        ax[1, 2].set_xlabel("time elapsed [sec]")
+        ax[1, 2].set_ylabel("yaw estimation error [rad]")
+        ax[1, 2].legend()
+
         plt.show()
+
+    def normalize_angles(self, angles):
+        """
+        Args:
+            angles (float or numpy.array): angles in radian (= [a1, a2, ...], shape of [n,])
+        Returns:
+            numpy.array or float: angles in radians normalized b/w/ -pi and +pi (same shape w/ angles)
+        """
+        angles = (angles + np.pi) % (2 * np.pi) - np.pi
+        return angles
 
     def plot3d(self):
         pass
@@ -48,4 +144,3 @@ class ResultVizPlotter(object):
 if __name__ == "__main__":
     # Unit Test Here
     pass
-
