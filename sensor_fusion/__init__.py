97035a7edafac84b320c83085b89a72c1c1c37a4
kimsooyoung <tge1375@naver.com>
2021-08-23 01:35:48 +0900

    Update : structure reordered

--- sensor_fusion/__init__.py
+++ sensor_fusion/__init__.py
@@ -1,1 +1,6 @@
 __all__ = ['dataset_mgmt', 'filter', 'slam', 'utils', 'visualize']
+
+from .filter import EKF
+from .visualize import Visualization
+from .dataset_mgmt import KittiDatasetMgmt
+from .dataset_mgmt.geo_utils import normalize_angles
