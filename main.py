b6223b97106fa6999c9557273aa4b57fab46e164
kimsooyoung <tge1375@naver.com>
2021-08-23 01:35:17 +0900

    Update : folder reoderded

--- main.py
+++ main.py
@@ -1,48 +1,65 @@
-import numpy as np
-from sensor_fusion.dataset_mgmt.geo_utils import normalize_angles
-from sensor_fusion.dataset_mgmt import KittiDatasetMgmt
-from sensor_fusion.filter import EKF
 import argparse
-from sensor_fusion.visualize import Visualization
+import numpy as np
 
-def main():
+from sensor_fusion import (
+    EKF,
+    Visualization,
+    KittiDatasetMgmt,
+    dataset_mgmt,
+    normalize_angles,
+)
+
+# temporal func for Readability during Development
+def setupDataset():
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
-        '--kitti_root_dir',
+        "--kitti_root_dir",
         type=str,
         default="D:\\2011_09_30_drive_0033_sync",
-        help='Root directory for loading Kitti dataset '
+        help="Root directory for loading Kitti dataset ",
     )
+
     parser.add_argument(
-        '--kitti_date',
+        "--kitti_date",
         type=str,
         default="2011_09_30",
-        help='Kitti dataset time information')
+        help="Kitti dataset time information",
+    )
 
     parser.add_argument(
-        '--kitti_drive',
+        "--kitti_drive", type=str, default="0033", help="Kitti dataset drive number"
+    )
+
+    parser.add_argument('--plot_data', dest='plot_data', action='store_true')
+    parser.add_argument('--no-plot_data', dest='plot_data', action='store_false')
+    parser.set_defaults(plot_data=True)
+
+    parser.add_argument(
+        "--filter",
         type=str,
-        default="0033",
-        help='Kitti dataset drive number')
-
-    parser.add_argument('--plot_data',
-                        type=bool,
-                        default=True,
-                        help='Device used for model inference.')
-
-    parser.add_argument('--filter',
-                        type=str,
-                        default='EKF',
-                        choices=['KF','EKF','UKF','PF','HINF','LIDAR-SLAM','VISION-SLAM'],
-                        help='Filter which you want to use.[KF,EKF,UKF,PF,HINF,LIDAR-SLAM,VISION-SLAM]')
+        default="EKF",
+        choices=["KF", "EKF", "UKF", "PF", "HINF", "LIDAR-SLAM", "VISION-SLAM"],
+        help="Filter which you want to use.[KF,EKF,UKF,PF,HINF,LIDAR-SLAM,VISION-SLAM]",
+    )
 
     args = parser.parse_args()
-    dataset_mgmt = KittiDatasetMgmt(args.kitti_root_dir, args.kitti_date, args.kitti_drive)
 
-    # Noise Addition!!
+    dataset_mgmt = KittiDatasetMgmt(
+        args.kitti_root_dir, args.kitti_date, args.kitti_drive
+    )
+    return dataset_mgmt, args
+
+
+def main():
+
+    # Data Initialization Operations
+    dataset_mgmt, args = setupDataset()
+
+    # GPS Gaussian Noise Addition!!
     dataset_mgmt.addGaussianNoiseToGPS()
 
-    # Plot Data
+    # Plot Data, You can Turn off this with argparse option
     if args.plot_data is True:
         viz = Visualization(dataset_mgmt)
         viz.plotGPStrajactory()
@@ -63,19 +80,15 @@ def main():
 
     data_len = len(ts)
 
+    # array to store estimated 2d pose [x, y, theta]
+    estimated_state = [x]
+    # array to store estimated error variance of 2d pose
+    estimated_var = [[P[0, 0], P[1, 1], P[2, 2]]]
+
     try:
         # initialize Kalman filter
         ekf = EKF(x, P)
-
-        # array to store estimated 2d pose [x, y, theta]
-        mu_x = [x[0], ]
-        mu_y = [x[1], ]
-        mu_theta = [x[2], ]
-
-        # array to store estimated error variance of 2d pose
-        var_x = [P[0, 0], ]
-        var_y = [P[1, 1], ]
-        var_theta = [P[2, 2], ]
+        
 
         t_last = 0.0
 
@@ -84,59 +97,45 @@ def main():
             dt = t - t_last
 
             # get control input `u = [v, omega] + noise`
-            u = np.array([
-                noisy_forward_velocities[t_idx],
-                noisy_yaw_rates[t_idx]
-            ])
+            u = np.array([noisy_forward_velocities[t_idx], noisy_yaw_rates[t_idx]])
 
             # because velocity and yaw rate are multiplied with `dt` in state transition function,
             # its noise covariance must be multiplied with `dt**2.`
-            R = R * (dt ** 2.)
+            R = R * (dt ** 2.0)
 
             # propagate!
             ekf.propagate(u, dt, R)
 
             # get measurement `z = [x, y] + noise`
-            z = np.array([
-                noisy_trajectory_xyz[0, t_idx],
-                noisy_trajectory_xyz[1, t_idx]
-            ])
+            z = np.array(
+                [noisy_trajectory_xyz[0, t_idx], noisy_trajectory_xyz[1, t_idx]]
+            )
 
             # update!
             ekf.measurementUpdate(z, Q)
 
-            # save estimated state to analyze later
-            mu_x.append(ekf.state[0])
-            mu_y.append(ekf.state[1])
-            mu_theta.append(normalize_angles(ekf.state[2]))
-
-            # save estimated variance to analyze later
-            var_x.append(ekf.cov[0, 0])
-            var_y.append(ekf.cov[1, 1])
-            var_theta.append(ekf.cov[2, 2])
+            # save results
+            estimated_state.append(ekf.state)
+            estimated_var.append([ekf.cov[0, 0], ekf.cov[1, 1], ekf.cov[2, 2]])
 
             t_last = t
 
-        mu_x = np.array(mu_x)
-        mu_y = np.array(mu_y)
-        mu_theta = np.array(mu_theta)
 
-        var_x = np.array(var_x)
-        var_y = np.array(var_y)
-        var_theta = np.array(var_theta)
+        estimated_state = np.array(estimated_state)
+        estimated_var = np.array(estimated_var)
 
-        print(mu_x.shape)
-        print(mu_y.shape)
-        print(mu_theta.shape)
+        # Visualize estimated results
+        if args.plot_data is True:
+            viz.getEstimatedValues(estimated_state, estimated_var)
+            viz.plotEstimatedTraj()
+            viz.plotEstimated2DStates()
 
         pass
     except Exception as e:
         print(e)
     finally:
         print(f"Done...")
-        if
 
 
 if __name__ == "__main__":
     main()
-
